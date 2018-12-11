1fe19f5fa05b3048c7c83cc103759b45b39811ad
Alexey <alexx333by@gmail.com>
2018-12-11 22:08:07 +0300

    Sniffer constructor implemented

--- WindowsSniffer/WindowsSniffer/Sniffer.cpp
+++ WindowsSniffer/WindowsSniffer/Sniffer.cpp
@@ -2,6 +2,7 @@
 
 #include <stdexcept>
 #include <ws2tcpip.h>
+#include <mstcpip.h>
 #include "Sniffer.h"
 
 namespace WindowsSniffer
@@ -14,9 +15,43 @@ namespace WindowsSniffer
 			throw new std::out_of_range("ppcCallback");
 		}
 
-		if (inet_pton(AF_INET, lpsIp, &m_iaIp) != 1)
+		IN_ADDR iaIp;
+		if (inet_pton(AF_INET, lpsIp, &iaIp) != 1)
 		{
 			throw new std::invalid_argument("lpsIp");
+		}
+
+		WSADATA wdData;
+		if (WSAStartup(MAKEWORD(2, 2), &wdData) != 0)
+		{
+			throw new std::runtime_error("Socket init error");
+		}
+
+		m_sListenSocket = socket(AF_INET, SOCK_RAW, IPPROTO_IP);
+		if (m_sListenSocket == INVALID_SOCKET)
+		{
+			WSACleanup();
+			throw new std::runtime_error("Error creating socket");
+		}
+
+		SOCKADDR_IN siAddress;
+		siAddress.sin_addr = iaIp;
+		siAddress.sin_family = AF_INET;
+		siAddress.sin_port = 0;
+
+		if (bind(m_sListenSocket, (sockaddr *)&siAddress, sizeof(SOCKADDR_IN)) == SOCKET_ERROR)
+		{
+			closesocket(m_sListenSocket);
+			WSACleanup();
+			throw new std::runtime_error("Error binding socket");
+		}
+
+		u_long ulArg = RCVALL_ON;
+		if (ioctlsocket(m_sListenSocket, SIO_RCVALL, &ulArg) == SOCKET_ERROR)
+		{
+			closesocket(m_sListenSocket);
+			WSACleanup();
+			throw new std::runtime_error("Error setting promiscuous mode");
 		}
 	}
 }
--- WindowsSniffer/WindowsSniffer/Sniffer.h
+++ WindowsSniffer/WindowsSniffer/Sniffer.h
@@ -20,7 +20,7 @@ namespace WindowsSniffer
 		PACKET_PROCESS_CALLBACK m_ppcCallback;
 		volatile BOOL m_bIsRunning;
 		HANDLE m_hProcessThread;
-		IN_ADDR m_iaIp;
+		SOCKET m_sListenSocket;
 
 		static DWORD WINAPI PacketProcessRoutine(PacketProcessRoutineArgument *lpInstance);
 	};
