7d772da58dcda4ce2ab52cbbc3877871aa97bc80
Alexey <alexx333by@gmail.com>
2018-12-11 14:01:54 +0300

    Library interface added

---
+++ WindowsSniffer/WindowsSniffer/Sniffer.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <Windows.h>
+
+namespace WindowsSniffer
+{
+	typedef void (PACKET_PROCESS_CALLBACK)(LPVOID lpSniffedData, DWORD dwByteCount);
+
+	class Sniffer
+	{
+	public:
+		Sniffer(PACKET_PROCESS_CALLBACK ppcCallback);
+		Sniffer(PACKET_PROCESS_CALLBACK ppcCallback, LPCSTR lpsIpAddress);
+		~Sniffer();
+		BOOL Start();
+		BOOL Stop();
+		BOOL Stop(DWORD dwWaitTime);
+	protected:
+		static LPCSTR GetLocalIp();
+	};
+}
