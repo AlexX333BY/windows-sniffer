83f77544abc7af88f1fadc87ae31814ca6bf04a2
Alexey <alexx333by@gmail.com>
2018-12-13 21:18:03 +0300

    Added generating protocol name for common protocols

--- WindowsSniffer/WindowsSniffer.View/IpPacketProcessor.cpp
+++ WindowsSniffer/WindowsSniffer.View/IpPacketProcessor.cpp
@@ -7,7 +7,7 @@ namespace WindowsSnifferView
 	LPSTR IpPacketProcessor::GetIpPacketStringRepresentation(WindowsSniffer::IP_HEADER *ipPacket)
 	{
 		CONST size_t sIpLen = 15;
-		CONST size_t sBufferLen = 128;
+		CONST size_t sBufferLen = 95;
 		LPSTR lpsResult = (LPSTR)calloc(sBufferLen + 1, sizeof(CHAR));
 		IN_ADDR iaAddress;
 
@@ -18,10 +18,27 @@ namespace WindowsSnifferView
 		inet_ntop(AF_INET, &iaAddress, lpsDest, sIpLen + 1);
 
 		sprintf_s(lpsResult, sBufferLen, "Source: %s Destination: %s Protocol: %s TTL: %d",
-			lpsSource, lpsDest, "TODO", ipPacket->bTimeToLive);
+			lpsSource, lpsDest, GetIpPacketProtocolStringRepresentation(ipPacket->bProtocol), ipPacket->bTimeToLive);
 
 		free(lpsSource);
 		free(lpsDest);
 		return (LPSTR)realloc(lpsResult, (lstrlen(lpsResult) + 1) * sizeof(CHAR));
+	}
+
+	LPCSTR IpPacketProcessor::GetIpPacketProtocolStringRepresentation(BYTE bProtocol)
+	{
+		switch (bProtocol)
+		{
+		case ICMP:
+			return "ICMP";
+		case TCP:
+			return "TCP";
+		case NVPII:
+			return "NVP-II";
+		case UDP:
+			return "UDP";
+		default:
+			return "UNKNOWN";
+		}
 	}
 }
--- WindowsSniffer/WindowsSniffer.View/IpPacketProcessor.h
+++ WindowsSniffer/WindowsSniffer.View/IpPacketProcessor.h
@@ -5,9 +5,18 @@
 
 namespace WindowsSnifferView
 {
+	typedef enum _IP_PROTOCOL
+	{
+		ICMP = 0x01,
+		TCP = 0x06,
+		NVPII = 0x0B,
+		UDP = 0x11
+	} IP_PROTOCOL;
+
 	class IpPacketProcessor
 	{
 	public:
 		static LPSTR GetIpPacketStringRepresentation(WindowsSniffer::IP_HEADER *ipPacket);
+		static LPCSTR GetIpPacketProtocolStringRepresentation(BYTE bProtocol);
 	};
 }
